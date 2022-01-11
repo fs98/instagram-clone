9739aded934234796e222fbcb7e600fddb610607
Fata Sefer <sefer.fata@gmail.com>
2022-01-11 22:21:49 +0100

    ui: add Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
+import Header from "./ui/Header";
 
 function App() {
-  return <h1 className="text-3xl font-bold">Instagram Clone</h1>;
+  return <Header/>  ;
 }
-
 export default App;
